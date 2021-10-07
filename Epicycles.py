40ff4fb54fe51a97cfd6b9d23b859b4e802b7486
AitorVadillo <82975584+AitorVadillo@users.noreply.github.com>
2021-10-07 20:10:21 +0200

    main

---
+++ Epicycles.py
@@ -0,0 +1,17 @@
+"""Provides a scripting component.
+
+    Inputs:
+        x: The x script variable
+        y: The y script variable
+        
+        
+    Output:
+        a: The a output variable"""
+
+
+
+import math
+import Rhino.Geometry as rg
+import System.Drawing.Color as Color
+import ghpythonlib.treehelpers as th
+import random as rnd
